a18f68e176e0f8f394f791d3aeaa1c66a93ee747
romanofficila <roman.shaon@gmail.com>
2022-12-15 15:41:52 +0600

    multiple image store and save public

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MultipleImage;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +12,19 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function StoreImg(Request $request){
+        if($files = $request->file('image')) {
+            // $title = $request->title;
+            foreach ($files as $file) {
+            // $path = $file->storeAs('featured-photos', md5(rand(1000, 10000)). '.' . $file->getClientOriginalExtension());
+            $path= $file->getClientOriginalName();
+            $file->move('featured-photos', $path);
+            MultipleImage::create([
+                             'image' => $path,
+                         ]);
+         
+        }
+    } 
+ }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Models\MultipleImage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::put('/multi/add', [Controller::class, 'StoreImg'])->name('store.image');
